98f0f1207c8238a4301e6b51a24bd4778fd2f11e
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-01-09 21:16:00 +0100

    impostato flex

--- teoria/libreria/src/components/AllTheBooks.jsx
+++ teoria/libreria/src/components/AllTheBooks.jsx
@@ -1,8 +1,7 @@
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+
 import fantasy from '../../src/books/fantasy.json';
-import Row from 'react-bootstrap/ListGroup';
-import Col from 'react-bootstrap/ListGroup';
+
+import { Image,Button,Row ,Col,Container,Card,ListGroup } from "react-bootstrap";
 /*uguale a*/
 //var somma = (x, y) => x + y; 
 
@@ -18,13 +17,14 @@ function AllTheBooks() {
   console.log(fantasy) 
   //book è il paramentro da map , il singolo elemento di fantasy lo metti a funzione e fai il return della funzione come un react cosi da leggere i libri fantasy
     return (
-              <Row>
+      <Container >
+              <Row >
         {fantasy.map(function (libri){
           console.log(libri)
           return( 
-          
-           <Col xs={12} md={4}  key={libri.asin}>
-        <Card style={{ width: '18rem' }}>
+          //key univoca affinchè non ci siano problemi nella riproduzione del map
+          <Col  xs={2} md={4} lg={6} key={libri.asin} >
+        <Card  style={{ width: '18rem' }}>
           <Card.Img variant="top" src={libri.img} />
             <Card.Body>
               <Card.Title>{libri.title}</Card.Title>
@@ -33,19 +33,20 @@ function AllTheBooks() {
               </Card.Text>
               </Card.Body>
               <ListGroup className="list-group-flush">
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                <ListGroup.Item>{libri.category}</ListGroup.Item>
+                <ListGroup.Item>{libri.price}</ListGroup.Item>
+                <ListGroup.Item>{libri.asin}</ListGroup.Item>
               </ListGroup>
                 <Card.Body>
-                  <Card.Link href="#">Card Link</Card.Link>
-                  <Card.Link href="#">Another Link</Card.Link>
+                <Button variant="primary">Buy</Button>
+                  <Button variant="primary">Remove</Button>
                 </Card.Body>
               </Card>
               </Col>)
 
-})}
+}  ) }
         </Row>
+        </Container>
         )
 }
 
