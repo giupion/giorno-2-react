b936a151208a35dc838813598db35167472805fd
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-01-14 22:33:59 +0100

    commenti da fare

--- teoria/libreria/src/components/CommentArea .jsx
+++ teoria/libreria/src/components/CommentArea .jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const CommentArea  = () => {
-  return (
-    <div>
+
+
+
+  
+export default class CommentArea  extends Component {
+
+    state={comments:[],}
+
+    componentDidMount = async () => {
+        try {
+         const response = await fetch("https://striveschool.herokuapp.com/api/comments")
+         
+            if(response.ok) {
+                 const data = await response.json()
+                  this.setState({ reservations: data })
+                
+       } else {
+           console.log('error while fetching')
+       }
+    
+    
+    }catch (error) {
+        console.log(error)
+        this.setState({ isLoading: false, isError: true })
+      }
+    }
+
       
-    </div>
-  )
+       
+    
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
-
-export default CommentArea 
